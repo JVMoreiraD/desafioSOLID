c24d00bcf835816d3484b394717b63403463b43f
João Vitor <jvitormoreirad@hotmail.com>
2022-06-05 14:53:13 -0300

    doc: swagger setup

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,9 @@
+/* eslint-disable prettier/prettier */
+import SwaggerUI from "swagger-ui-express";
+import swaggerFile from "swagger.json"
+
 import { app } from ".";
+
+app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is running!"));
